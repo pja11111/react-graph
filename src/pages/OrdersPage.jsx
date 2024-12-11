d5d4e9e994260373ab784ea076b3ce1e33e86fbc
thanaspon <thanaspon.kalaram@messagespring.com>
2024-12-11 11:24:38 +0700

    Update Order

--- src/pages/OrdersPage.jsx
+++ src/pages/OrdersPage.jsx
@@ -21,7 +21,7 @@ const OrdersPage = () => {
 
 			<main className='max-w-7xl mx-auto py-6 px-4 lg:px-8'>
 				<motion.div
-					className='grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-4 mb-8'
+					className='grid grid-cols-2 gap-5 sm:grid-cols-2 lg:grid-cols-4 mb-8'
 					initial={{ opacity: 0, y: 20 }}
 					animate={{ opacity: 1, y: 0 }}
 					transition={{ duration: 1 }}
